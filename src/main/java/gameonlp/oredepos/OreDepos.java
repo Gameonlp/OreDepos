e452cd6d068c196ed614c3f5b7a4ea247c67c0d5
Gameonlp <linq@gmx.de>
2022-04-20 14:40:57 +0200

    Added Icon and synced version

--- src/main/java/gameonlp/oredepos/OreDepos.java
+++ src/main/java/gameonlp/oredepos/OreDepos.java
@@ -3,11 +3,7 @@ package gameonlp.oredepos;
 import gameonlp.oredepos.blocks.miner.MinerScreen;
 import gameonlp.oredepos.compat.TOPCompat;
 import gameonlp.oredepos.net.PacketManager;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.ScreenManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -16,15 +12,10 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("oredepos")
