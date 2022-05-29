ae3eb939b99e35752d55c865474edd7e2ee326c6
Gameonlp <linq@gmx.de>
2022-05-29 21:24:46 +0200

    Made Drill enchantable

--- src/main/java/gameonlp/oredepos/items/DrillHeadItem.java
+++ src/main/java/gameonlp/oredepos/items/DrillHeadItem.java
@@ -3,6 +3,9 @@ package gameonlp.oredepos.items;
 import net.minecraft.world.item.Item;
 
 import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class DrillHeadItem extends Item {
     private final Item corresponding;
@@ -13,5 +16,17 @@ public class DrillHeadItem extends Item {
 
     public Item getCorresponding() {
         return corresponding;
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        if(enchantment.equals(Enchantments.BLOCK_FORTUNE) || enchantment.equals(Enchantments.SILK_TOUCH)){
+            return true;
+        }
+        return super.canApplyAtEnchantingTable(stack, enchantment);
+    }
+    @Override
+    public boolean isEnchantable(ItemStack p_41456_) {
+        return true;
     }
 }
